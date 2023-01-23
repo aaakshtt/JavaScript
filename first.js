b602662738ef3bb5d32ea0e7157e1045a629e775
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2023-01-23 18:49:48 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,3 +1,6 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
 var a = 23;
 var b = 35;
 var answer;
@@ -10,7 +13,14 @@ else{
 console.log(answer);
 var pens;
 pens = ["red" ,  "blue" , "green" , 19 , 42];
-console.log("Before:  " + pens);
-pens[2] = " black";
-console.log("After: " + pens);
-console.log(pens[3]);
+// console.log("Before:  " + pens);
+// pens[2] = " black";
+// console.log("After: " + pens);
+// console.log(pens[3]);
+// console.log("Lenght : " +pens.length);
+// console.log("Reverse : " +pens.reverse());      //Reverses the array
+// pens.shift();       //Removes the first item of the arrry
+// pens.pop();         //Removes the last item
+// pens.slice(positon , n)      //Slices the array from position and n number
+var result = pens.indexOf("blue");
+console.log(result);
