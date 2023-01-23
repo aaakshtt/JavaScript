b44b14619f8255ec8f665047f1b8a773898b736e
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2023-01-23 18:16:47 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,16 @@
+var a = 23;
+var b = 35;
+var answer;
+if(a==b){
+    answer = true;
+}
+else{
+    answer  = false;
+}
+console.log(answer);
+var pens;
+pens = ["red" ,  "blue" , "green" , 19 , 42];
+console.log("Before:  " + pens);
+pens[2] = " black";
+console.log("After: " + pens);
+console.log(pens[3]);
