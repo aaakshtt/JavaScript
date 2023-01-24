5d76243fc6ebfd7925410195dcaa1e2a42230d40
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2023-01-24 18:33:56 +0530

    Various function created and executed

---
+++ functions.js
@@ -0,0 +1,19 @@
+console.log("Welcome to Programiz!");
+function multiply(a,b){
+    var a ,b;
+    a= 14;
+    b=15;
+    var result = a+b;
+    console.log(result);
+    return result;
+}
+var multiplied = multiply(3,4);
+
+function biggestfunction(a,b){
+    a>b ? console.log("A : " ,a) : console.log("B ", b);
+}
+var  a = 3/4;
+var b = 5/7;
+biggestfunction();       Execute the function
+biggestfunction(10,14);
+biggestfunction(54,46);
