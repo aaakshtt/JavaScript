5ca5c98e08d65e66a4a61e1e4f9d7b814831f04a
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2023-01-24 18:48:31 +0530

    Create anonymousfunctions.js

---
+++ anonymousfunctions.js
@@ -0,0 +1,11 @@
+var a= 5/7;
+var b= 9/2;
+
+var thebiggest  = function(){
+    var result; 
+    a>b ? result = ["a ",a] : result = ["b ",b ];
+    console.log(result);
+    // return result;
+}
+thebiggest();
+// console.log(thebiggest());
